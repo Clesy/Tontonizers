3e8cf5c614fe70710c4b0937a9a08cccebaaf0e8
Clesy <gerasum04@gmail.com>
2020-10-06 00:49:38 +0300

    add slider

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    /*AUTO SLIDER */
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 20000);
+
+
+}
