3596261d2886948432d9385583c051fece462219
Evgeniy <gerasum04@gmail.com>
2020-10-11 15:50:15 +0300

    add comment, intro background and banner

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,7 @@
-$('.menu-btn').on('click', function(e) {
-    e.preventDefault;
-    $(this).toggleClass('menu-btn_active')
-    $('.menu').toggleClass('menu-active')
+$(document).ready(function() {
+    $('.menu-btn').on('click', function(e) {
+        e.preventDefault;
+        $(this).toggleClass('menu-btn_active')
+        $('.menu').toggleClass('menu-active')
+    });
 });
-
-// function openNav() {
-//     document.getElementById("mySidebar").style.width = "250px";
-//     document.getElementById("main").style.marginLeft = "250px";
-// }
-
-// function closeNav() {
-//     document.getElementById("mySidebar").style.width = "0";
-//     document.getElementById("main").style.marginLeft = "0";
-// }
